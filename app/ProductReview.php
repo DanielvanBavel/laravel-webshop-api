c302bbf69eda103ae7bc7268aca3a017fe842b23
DanielvanBavel <daniel.ajax.1996@hotmail.com>
2018-10-21 15:54:46 +0200

    project structure

---
+++ app/ProductReview.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Product;
+
+class ProductReview extends Model 
+{
+    /*
+    * set database table name
+    */
+    protected $table = "productReviews";
+
+    /*
+    * Disable created_at and updated_at in table
+    */
+    public $timestamps = false;
+
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 
+        'date',
+        'description'
+    ];
+
+    public function products() {
+        return $this->belongsTo(Product::class);
+    }
+}
